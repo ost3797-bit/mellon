74b1e89bb360b62b3929e55d7f2fdec74160d3a7
Melonny <ost3797@gmail.com>
2025-09-02 16:21:58 +0900

    Add files via upload

---
+++ js/firebaseConfig.js
@@ -0,0 +1,8 @@
+window.__FIREBASE_CONFIG__ = {
+apiKey: "AIzaSyD28Qspp39iz_RWsiCtOlHJ_Bmjx4VEcUI",
+authDomain: "melon1-3ba00.firebaseapp.com",
+projectId: "melon1-3ba00",
+storageBucket: "melon1-3ba00.firebasestorage.app",
+messagingSenderId: "742565966130",
+appId: "1:742565966130:web:0d31a4bbc93882a0b9e72b"
+}
